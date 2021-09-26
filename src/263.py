731322fb0430f8623dd777067192b31d3acd54dc
Chintsai Hwo <jcglqmoyx@gmail.com>
2021-09-26 23:46:51 +0800

    update

---
+++ src/263.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n != 1:
+            while n % 2 == 0:
+                n >>= 1
+            while n % 3 == 0:
+                n /= 3
+            while n % 5 == 0:
+                n /= 5
+            break
+        return n == 1
